67ff5f3a19992ef2f011420a4792c8067270fcaf
Alexey <deal110988@him-mail.ch>
2025-02-05 19:52:31 +0100

    .. pg 119 ..

--- classes.cpp
+++ classes.cpp
@@ -55,6 +55,36 @@ public:
     }
 };
 
+// Constructor function must have the same name as the class, then it becomes a constructor
+// Constructor with no parameters or with default parameters is a Default Constructor
+// I'm also noticing that we do not specify a return type for Constructor, since they don't have it
+// If a default constructor is not explicitly specified in the code, compiler will generate it
+
+class MyClassWithConstructor
+{
+public:
+    MyClassWithConstructor()
+    {
+        std::cout << "Default constructor invoked \n";
+    }
+};
+
+// And here's a class with a constructor that actually does something:
+
+class MyClassWithLessUselessConstructor
+{
+public:
+    int a, b;
+
+    MyClassWithLessUselessConstructor(int aaa, int bbb)
+    {
+        a = aaa;
+        b = bbb;
+
+        std::cout << "Here they are: " << a << " --- " << b << '\n';
+    }
+};
+
 int main()
 {
     // hmmm, to create an instance of a class we don't need new, just hekk:
@@ -63,4 +93,10 @@ int main()
     MyAllPublicClass mypub;
     mypub.x = 213;
     mypub.printX();
+
+    MyClassWithConstructor myclasswconstruct;
+
+    MyClassWithLessUselessConstructor lessuseless(3, 5);
+
+    std::cout << "Let's check value of a in the instance of a class: " << lessuseless.a << '\n';
 }
